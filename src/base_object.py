4e27e82e28af703fd0e2409bc35c158af1bd5676
jesse <jesse.koekkoek@incentro.com>
2019-05-11 14:01:49 +0200

    Added BaseObject

---
+++ src/base_object.py
@@ -0,0 +1,12 @@
+import sys, abc
+
+from abc import ABC, abstractmethod
+
+class BaseObject(ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def draw(self):
+        pass
