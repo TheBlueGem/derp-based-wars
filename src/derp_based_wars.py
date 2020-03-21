d0d3132305498c9d27a46de4739ace0e636d24ea
jesse <jesse.koekkoek@incentro.com>
2020-03-21 11:32:06 +0100

    Put moving in its own method in main file for now

--- src/derp_based_wars.py
+++ src/derp_based_wars.py
@@ -3,7 +3,7 @@ import sys
 from pygame import quit as pygame_quit
 from pygame import display
 from pygame import init
-from pygame.locals import QUIT, KEYUP, K_ESCAPE, KEYDOWN, K_LEFT
+from pygame.locals import QUIT, KEYUP, K_ESCAPE, KEYDOWN
 from pygame import time
 from pygame import event as pygame_event
 
@@ -13,16 +13,15 @@ from units.unitFactory import UnitFactory
 from common import WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND_COLOR
 from selector import Selector
 
+board_width = 10
+board_height = 10
+
 
 # Main game loop
 def main():
     init()
     fps_clock = time.Clock()
     display_surf = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-    board_width = 10
-    board_height = 10
-
     display.set_caption('Derp Based Wars')
 
     main_board = Board(width=board_width, height=board_height)
@@ -44,20 +43,10 @@ def main():
                 pygame_quit()
                 sys.exit()
 
-            if event.type == KEYDOWN and event.key == SELECTOR_LEFT and selector.get_selected()[0] is not 1:
-                selector.move(-1, 0)
-                print(selector)
-            if event.type == KEYDOWN and event.key == SELECTOR_RIGHT and selector.get_selected()[0] is not board_width:
-                selector.move(1, 0)
-                print(selector)
-            if event.type == KEYDOWN and event.key == SELECTOR_UP and selector.get_selected()[1] is not 1:
-                selector.move(0, -1)
-                print(selector)
-            if event.type == KEYDOWN and event.key == SELECTOR_DOWN and selector.get_selected()[1] is not board_height:
-                selector.move(0, 1)
-                print(selector)
+            if event.type == KEYDOWN:
+                handle_keydown_event(event.key, selector)
 
-        # Main drawing loop
+                # Main drawing loop
         new_surf = display_surf.copy()
         new_surf.fill(BACKGROUND_COLOR)
         main_board.draw(new_surf)
@@ -65,6 +54,21 @@ def main():
         display_surf.blit(new_surf, (0, 0))
         display.update()
         fps_clock.tick(FPS)
+
+
+def handle_keydown_event(event_key, selector):
+    if event_key == SELECTOR_LEFT and selector.get_selected()[0] is not 1:
+        selector.move(-1, 0)
+        print(selector)
+    if event_key == SELECTOR_RIGHT and selector.get_selected()[0] is not board_width:
+        selector.move(1, 0)
+        print(selector)
+    if event_key == SELECTOR_UP and selector.get_selected()[1] is not 1:
+        selector.move(0, -1)
+        print(selector)
+    if event_key == SELECTOR_DOWN and selector.get_selected()[1] is not board_height:
+        selector.move(0, 1)
+        print(selector)
 
 
 if __name__ == '__main__':
